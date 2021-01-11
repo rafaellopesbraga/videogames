5ca322add49f319c2b06d644a7b26e0a1d9f66b1
Rafael Lopes Braga <rafaellopesbraga@gmail.com>
2021-01-11 19:22:30 -0300

    Using IGDB API

--- config/services.php
+++ config/services.php
@@ -29,5 +29,9 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'igdb' => [
+        'Client-ID' => env('IGDB_CLIENT_ID'),
+        'Authorization' => env('IGDB_AUTH_TOKEN')
+    ]
 
 ];
